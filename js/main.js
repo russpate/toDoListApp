50d21fd729fb5bec30cf2b8f48a5f39c4797ff33
Russell Pate <russ@russpate.com>
2016-02-25 09:07:21 -0500

    initial refactor commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,81 @@
-// $(document).ready(function() {
-//   toDoPage.init();
-// });
-// var toDoPage = {
-//   init: function() {
-//     toDoPage.styling();
-//     toDoPage.events();
-//   },
-//   styling: function() {
-//   },
-//   events: function() {
-//   },
-//
-//
-// }; // var toDoPage - everything goes in here
+$(document).ready(function() {
+  toDoPage.init();
+});
+var toDoPage = {
+  init: function() {
+    toDoPage.styling();
+    toDoPage.events();
+  },
+  styling: function() {
+    toDoPage.addAllItems(listData);
+    $('.toDoFooter').html(templates.nav);
+  },
+  events: function() {
+    $('form').on('submit', function (event) {
+      event.preventDefault();
+      var newItem = toDoPage.getPostFromDom();
+        toDoPage.addItem(newItem);
+        toDoPage.addAllItems(toDoPage.getItem());
+        $('input').val('');
+    });
+    $(".toDoFooter").find("a").on("click", function(event) {
+      event.preventDefault();
+      var textClicked = $(this).text();
+      var selectedState = "." + $(this).attr("rel");
+      $(selectedState).addClass("current");
+      $(selectedState).siblings().removeClass("current");
+        if(textClicked === "Active") {
+          var notCompleted = listData.filter(function(el) {
+            return el.complete !== true;
+          });
+          toDoPage.addAllItems(notCompleted);
+        } else if(textClicked === "All") {
+          toDoPage.addAllItems(listData);
+        } else {
+          var completed = listData.filter(function(el) {
+            return el.complete === true;
+          });
+          toDoPage.addAllItems(completed);
+        }
+    });
+    $('.listBox').on('click','input[type="checkbox"]', function() {
+      var index = $(this).parent().data('idx');
+      listData[index].complete = !listData[index].complete;
+      $(this).siblings('p').toggleClass('complete');
+    });
+  },
+  getItem: function(){
+    return listData;
+  },
+  addItem: function(newItem){
+      listData.push(newItem);
+  },
+  deleteItem: function(idx) {
+    listData.splice(idx, 1);
+  },
+  addItemToDom: function(itemData, templateStr, $target) {
+      var tmpl = _.template(templateStr);
+      $target.append(tmpl(itemData));
+  },
+  addAllItems: function(arr) {
+    $('section').html('');
+    _.each(arr, function (el, idx) {
+      // defines idx with the index of the element
+      el.idx = idx;
+      toDoPage.addItemToDom(el, templates.item, $('section'));
+    });
+  },
+getPostFromDom:  function() {
+    var content = $('input[name="content"]').val();
+    return {
+      content: content,
+      complete: false
+    };
+  },
+
+
+
+}; // var toDoPage - everything goes in here
 
 
 
@@ -22,75 +85,75 @@
 
 
 
-// this getItem function refers to the object listData
-// where the content being created by the input
-// is being stored.
-function getItem() {
-  return listData;
-}
+// // this getItem function refers to the object listData
+// // where the content being created by the input
+// // is being stored.
+// function getItem() {
+//   return listData;
+// }
 
 
-// addItem adds the newly created list item to the end
-// of the listData object
-function addItem(newItem) {
-  listData.push(newItem);
-}
+// // addItem adds the newly created list item to the end
+// // of the listData object
+// function addItem(newItem) {
+//   listData.push(newItem);
+// }
 
 
-// deleteItem uses splice to remove listItems.. not sure
-// maybe I can use this for clear?
-function deleteItem(idx) {
-  listData.splice(idx, 1);
-}
+// // deleteItem uses splice to remove listItems.. not sure
+// // maybe I can use this for clear?
+// function deleteItem(idx) {
+//   listData.splice(idx, 1);
+// }
 
 
-// I'm not really sure how this adds items to the DOM
-function addItemToDom(itemData, templateStr, $target) {
-    var tmpl = _.template(templateStr);
-    $target.append(tmpl(itemData));
-}
+// // I'm not really sure how this adds items to the DOM
+// function addItemToDom(itemData, templateStr, $target) {
+//     var tmpl = _.template(templateStr);
+//     $target.append(tmpl(itemData));
+// }
+
+// function addAllItems(arr) {
+//   $('section').html('');
+//   _.each(arr, function (el, idx) {
+//     // defines idx with the index of the element
+//     el.idx = idx;
+//     addItemToDom(el, templates.item, $('section'));
+//   });
+// }
 
 
-//
-function addAllItems(arr) {
-  $('section').html('');
-  _.each(arr, function (el, idx) {
-    // defines idx with the index of the element
-    el.idx = idx;
-    addItemToDom(el, templates.item, $('section'));
-  });
-}
-
-
-//checks the value of the input and returns what has
-// been entered.
-function getPostFromDom() {
-  var content = $('input[name="content"]').val();
-  return {
-    content: content,
-    complete: false
-  };
-}
+// //checks the value of the input and returns what has
+// // been entered.
+// function getPostFromDom() {
+//   var content = $('input[name="content"]').val();
+//   return {
+//     content: content,
+//     complete: false
+//   };
+// }
 
 
 // this retrieves the information from the
 // input, adds it to the object, then refreshes
 // the page with the newly created content, maybe?
-addAllItems(listData);
-$('form').on('submit', function (event) {
-  event.preventDefault();
-  var newItem = getPostFromDom();
-    addItem(newItem);
-    addAllItems(getItem());
-    $('input').val('');
-});
 
-// this deletes items from the list
-$('section').on('click', '.delete', function (event) {
-  var idx = $(this).closest('article').data('idx');
-  deleteItem(idx);
-  addAllItems(getItem());
-});
+// addAllItems(listData);
+
+// $('form').on('submit', function (event) {
+//   event.preventDefault();
+//   var newItem = getPostFromDom();
+//     addItem(newItem);
+//     addAllItems(getItem());
+//     $('input').val('');
+// });
+
+// // this deletes items from the list
+// $('section').on('click', '.delete', function (event) {
+//   var idx = $(this).closest('article').data('idx');
+//   deleteItem(idx);
+//   addAllItems(getItem());
+// });
 
 // maybe this will delete allComplete?
 $('.clearComplete').on('click', '.delete', function (event) {
@@ -104,42 +167,39 @@ $('.clearComplete').on('click', '.delete', function (event) {
   addAllItems(getItem());
 });
 
-// adds footer nav to page
-$('.toDoFooter').html(templates.nav);
+// // adds footer nav to page
+// $('.toDoFooter').html(templates.nav);
 
-// clicks for tabs to switch between todo states
-$(".toDoFooter").find("a").on("click", function(event) {
-  event.preventDefault();
-  var textClicked = $(this).text();
-  var selectedState = "." + $(this).attr("rel");
-  $(selectedState).addClass("current");
-  $(selectedState).siblings().removeClass("current");
+// // clicks for tabs to switch between todo states
+// $(".toDoFooter").find("a").on("click", function(event) {
+//   event.preventDefault();
+//   var textClicked = $(this).text();
+//   var selectedState = "." + $(this).attr("rel");
+//   $(selectedState).addClass("current");
+//   $(selectedState).siblings().removeClass("current");
+//     if(textClicked === "Active") {
+//       var notCompleted = listData.filter(function(el) {
+//         return el.complete !== true;
+//       });
+//       console.log("Active ACTIVE", notCompleted);
+//       addAllItems(notCompleted);
+//     } else if(textClicked === "All") {
+//       console.log('ALL', listData);
+//       addAllItems(listData);
+//     } else {
+//       var completed = listData.filter(function(el) {
+//         return el.complete === true;
+//       });
+//       console.log('completed', completed);
+//       addAllItems(completed);
+//     }
+// });
 
-  // filters the bottom nav based on whether
-  // or not the completed equals true
-    if(textClicked === "Active") {
-      var notCompleted = listData.filter(function(el) {
-        return el.complete !== true;
-      });
-      console.log("Active ACTIVE", notCompleted);
-      addAllItems(notCompleted);
-    } else if(textClicked === "All") {
-      console.log('ALL', listData);
-      addAllItems(listData);
-    } else {
-      var completed = listData.filter(function(el) {
-        return el.complete === true;
-      });
-      console.log('completed', completed);
-      addAllItems(completed);
-    }
-});
-
-$('.listBox').on('click','input[type="checkbox"]', function() {
-  var index = $(this).parent().data('idx');
-  listData[index].complete = !listData[index].complete;
-  $(this).siblings('p').toggleClass('complete');
-});
+// $('.listBox').on('click','input[type="checkbox"]', function() {
+//   var index = $(this).parent().data('idx');
+//   listData[index].complete = !listData[index].complete;
+//   $(this).siblings('p').toggleClass('complete');
+// });
 
 
 
